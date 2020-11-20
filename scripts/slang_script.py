80a55ff7ca0e8709df857bab3b378234c7474911
alejovallega <63580466+alejovallega@users.noreply.github.com>
2020-11-20 10:43:56 -0600

    Add files via upload

---
+++ scripts/slang_script.py
@@ -0,0 +1,28 @@
+#Author for this script and slang.txt: https://github.com/rishabhverma17/sms_slang_translator/blob/master/slang.txt
+import csv
+import re
+
+
+
+def translator(user_string):
+    user_string = user_string.split(" ")
+    j = 0
+    for _str in user_string:
+        # File path which consists of Abbreviations.
+        fileName = "G:\\COMP4710_Project\\slang_converter\slang.txt"
+        # File Access mode [Read Mode]
+        accessMode = "r"
+        with open(fileName, accessMode) as myCSVfile:
+            # Reading file as CSV with delimiter as "=", so that abbreviation are stored in row[0] and phrases in row[1]
+            dataFromFile = csv.reader(myCSVfile, delimiter="=")
+            # Removing Special Characters.
+            _str = re.sub('[^a-zA-Z0-9-_.]', '', _str)
+            for row in dataFromFile:
+                # Check if selected word matches short forms[LHS] in text file.
+                if _str.upper() == row[0]:
+                    # If match found replace it with its appropriate phrase in text file.
+                    user_string[j] = row[1]
+            myCSVfile.close()
+        j = j + 1
+    # Replacing commas with spaces for final output.
+    return ' '.join(user_string)
