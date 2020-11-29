af62ea208d55446818994cbcf1e9b6e0bb96746d
Devin Efendy <devinefendy.dev@gmail.com>
2020-11-29 13:19:07 -0600

    Add feature extraction script

---
+++ scripts/tf_idf_helper.py
@@ -0,0 +1,45 @@
+import re
+
+
+def pre_process(text):
+
+    # lowercase
+    text = text.lower()
+
+    # remove tags
+    text = re.sub("", "", text)
+
+    # remove special characters and digits
+    text = re.sub("(\\d|\\W)+", " ", text)
+
+    return text
+
+
+def sort_coo(coo_matrix):
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+
+
+def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+    """get the feature names and tf-idf score of top n items"""
+
+    # use only topn items from vector
+    sorted_items = sorted_items[:topn]
+
+    score_vals = []
+    feature_vals = []
+
+    # word index and corresponding tf-idf score
+    for idx, score in sorted_items:
+
+        # keep track of feature name and its corresponding score
+        score_vals.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+
+    # create a tuples of feature,score
+    #results = zip(feature_vals,score_vals)
+    results = {}
+    for idx in range(len(feature_vals)):
+        results[feature_vals[idx]] = score_vals[idx]
+
+    return results
